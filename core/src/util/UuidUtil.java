0be9e59006909a78c0aaf619e42ffb087759b148
刘鹏 <13405006821@139.com>
2016-12-13 17:03:35 +0800

    11

---
+++ core/src/util/UuidUtil.java
@@ -0,0 +1,10 @@
+package util;
+
+import java.util.UUID;
+
+public class UuidUtil {
+	public static String get32UUID() {
+		String uuid = UUID.randomUUID().toString().trim().replaceAll("-", "");
+		return uuid;
+	}
+}
