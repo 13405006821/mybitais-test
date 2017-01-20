7f725666904c8393d901b31d1726bfe40ac7278c
liupeng <13405006821@139.com>
2017-01-20 22:46:30 +0800

    1

--- core/src/base/controller/BaseController.java
+++ core/src/base/controller/BaseController.java
@@ -1,35 +1,71 @@
 package base.controller;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.alibaba.fastjson.JSON;
 
 import model.PageData;
 
 public class BaseController {
 	protected Logger logger = Logger.getLogger(getClass());
 
-	@Autowired
 	private HttpServletRequest request;
+	private HttpServletResponse response;
 
 	public PageData getPageData() {
-		return new PageData(getRequest());
+		return new PageData(request);
+	}
+
+	@ModelAttribute
+	public void setReqAndRes(HttpServletRequest request, HttpServletResponse response) {
+		this.request = request;
+		this.response = response;
+	}
+
+	/**
+	 * 将对象转换成JSON字符串，并响应回前台
+	 * 
+	 * @param object
+	 * @throws IOException
+	 */
+	public void writeJson(Object object) {
+		try {
+			String json = JSON.toJSONStringWithDateFormat(object, "yyyy-MM-dd HH:mm:ss");
+			getResponse().setContentType("text/json;charset=utf-8");
+			getResponse().getWriter().write(json);
+			getResponse().getWriter().flush();
+			getResponse().getWriter().close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String getBasePath() {
+		String path = request.getContextPath();
+		String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + path
+				+ "/";
+		return basePath;
+	}
+
+	public HttpServletResponse getResponse() {
+		return response;
+	}
+
+	public void setResponse(HttpServletResponse response) {
+		this.response = response;
 	}
 
 	public HttpServletRequest getRequest() {
-		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
-				.getRequestAttributes()).getRequest();
-
 		return request;
 	}
-	
-	public String getBasePath() {
-		String path = request.getContextPath();
-		String basePath = request.getScheme() + "://" + request.getServerName()
-				+ ":" + request.getServerPort() + path + "/";
-		return basePath;
+
+	public void setRequest(HttpServletRequest request) {
+		this.request = request;
 	}
 }
