2d9813e43cf016fae7f643a5a56b92df5db34533
刘鹏 <13405006821@139.com>
2017-01-20 18:28:05 +0800

    1

--- core/src/base/aop/MyExceptionHandler.java
+++ core/src/base/aop/MyExceptionHandler.java
@@ -1,31 +1,40 @@
 package base.aop;
 
-import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
-import org.apache.log4j.Logger;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.alibaba.fastjson.JSON;
 
 import model.Json;
 import util.SysConstants;
 
 /**
  * 统一报错处理
+ * 
  * @author liup
  *
  */
-public class MyExceptionHandler {
-	
-	protected Logger logger = Logger.getLogger(getClass());
-	
-	@ExceptionHandler
-	@ResponseBody
-	public Json exceptionHandler(HttpServletRequest request, Exception ex) {
-		logger.error(ex.getMessage());
-		ex.printStackTrace();
+public class MyExceptionHandler implements HandlerExceptionResolver {
+
+	@Override
+	public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object object, Exception exception) {
+		exception.printStackTrace();
 		Json json = new Json();
 		json.setSuccess(false);
 		json.setMsg(SysConstants.SYS_ERROR);
-		return json;
+		response.setContentType("text/json;charset=utf-8");
+		try {
+			response.getWriter().write(JSON.toJSONString(json));
+			response.getWriter().flush();
+			response.getWriter().close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
