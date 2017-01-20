8230f340b64c456ae9f051eb18d2253a3d33f310
刘鹏 <13405006821@139.com>
2017-01-20 17:10:19 +0800

    1

--- core/src/util/PublicUtil.java
+++ core/src/util/PublicUtil.java
@@ -2,9 +2,14 @@ package util;
 
 import java.util.UUID;
 
-public class UuidUtil {
+public class PublicUtil {
+
 	public static String get32UUID() {
 		String uuid = UUID.randomUUID().toString().trim().replaceAll("-", "");
 		return uuid;
+	}
+
+	public static int getPageStartIndex(String page, String pageSize) {
+		return (Integer.valueOf(page) - 1) * Integer.valueOf(pageSize);
 	}
 }
